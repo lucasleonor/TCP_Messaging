92a1b51be95e2f28906f48ca457d8217069a4c7d
Lucas Leonor <lucas.g.leonor@gmail.com>
2020-09-06 16:02:45 -0300

    Extracted Server and Client into different projects

--- Client/src/main/java/br/com/training/threads/messaging/client/MessagingClientRunner.java
+++ Client/src/main/java/br/com/training/threads/messaging/client/MessagingClientRunner.java
@@ -1,6 +1,5 @@
 package br.com.training.threads.messaging.client;
 
-import br.com.training.threads.messaging.server.MessagingServer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
